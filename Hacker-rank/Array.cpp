c5e4742283fd0160b0b3b7ed6cb33fb5993ffc29
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2025-01-08 12:05:05 +0530

    Q3

---
+++ Hacker-rank/Array.cpp
@@ -0,0 +1,81 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #include <iostream>
+// #include <sstream>
+// #include <vector>
+
+// using namespace std;
+
+
+// int main(){
+//     int size;
+//     cin >> size ;
+//     cin.ignore();
+     
+//     string str ;
+//     getline(cin, str);
+
+//     stringstream ss(str);
+
+//     //use vector for dynamic array
+//     vector<int> array;
+
+//     int value;
+//     while(ss >> value){
+//         array.push_back(value);
+//     }
+
+//     for(int i =  array.size() -1 ; i>=0;i--){
+//         cout << array[i] << " ";
+//     }
+
+//     return 0;
+
+
+// }
+
+
+
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main(){
+//     int size;
+//     cin >> size;
+
+//     int array[size];
+    
+//     for(int i = 0;i <size;++i){
+//         cin >> array[i];
+//     }
+
+//     // int size = sizeof(array)/sizeof(array[0]);
+
+//     for(int i = size - 1;i>=0;i--){
+//         cout << array[i] << " ";
+//     }
+
+//     return 0;
+// }
+
