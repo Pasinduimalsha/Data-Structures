28961fcc0894000db900f1846839a04dbd0761dd
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2025-01-08 11:17:23 +0530

    2nd

--- Basic.cpp
+++ Basic.cpp
@@ -3,6 +3,21 @@
 using namespace std;
 
 
+int number(int a[], int size){
+    for(int i=0;i<size;i++){
+        cout << a[i]<< " ";
+    }
+
+    return 0;
+}
+
+//  If we want to return an array in a function, we can do it by returning a pointer to the array or by returning the array itself.
+
+int *number1(){
+    int static numbers[5] = {1,2,3,4,5};
+    return numbers;
+}
+
 int main(){
     int array[2][5];
    
@@ -13,9 +28,7 @@ int main(){
             cin >> array[i][j];
         }
         cout << endl;
-
     }
-
     cout << "The elements of the array are: \n";
 
     for (int i = 0;i<2;i++){
@@ -24,8 +37,28 @@ int main(){
         }
         cout << endl;
     }
-
-    return 0;
     
+    cout << endl;
+
+    int array1[] ={1,2,3,4,5};
+
+    int size = sizeof(array1)/sizeof(array1[4]);
+    cout << "The size of the array is: "<< size << endl;
+
+    number(array1, size);
+    
+    cout << endl;   
+    cout<<  "Returning array in the function" << endl;
+
+    int *p = number1();
+    cout << p;
+
+    cout << "The elements of the array are: \n";
+    for(int i=0;i<5;i++){
+        cout << p[i] << " ";
+    }
+
+    return 0;   
 }
+
 
